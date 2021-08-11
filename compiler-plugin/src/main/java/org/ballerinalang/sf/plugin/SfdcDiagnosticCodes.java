7e179d81d101736fb5eea059c2df5f399bad373d
sachinira <sachipcranasinghe@gmail.com>
2021-08-11 07:39:35 +0530

    Add the compiler plugin implementation and tests

---
+++ compiler-plugin/src/main/java/org/ballerinalang/sf/plugin/SfdcDiagnosticCodes.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.ballerinalang.sf.plugin;
+
+import io.ballerina.tools.diagnostics.DiagnosticSeverity;
+
+import static io.ballerina.tools.diagnostics.DiagnosticSeverity.ERROR;
+
+public enum SfdcDiagnosticCodes {
+    SFDC_101("SFDC_101", "invalid annotation type '%s'", ERROR),
+    SFDC_102("SFDC_102", "invalid method name '%s': expected one of the following types: " + "'onCreate', 'onUpdate', 'onDelete', 'onRestore'", ERROR),
+    SFDC_103("SFDC_103", "%s parameter list", ERROR),
+    SFDC_104("SFDC_104", "%s parameters in the parameter list", ERROR),
+    SFDC_105("SFDC_105", "invalid parameter type '%s'", ERROR),
+    SFDC_106("SFDC_106", "qualifier name required: '%s'", ERROR),
+    SFDC_107("SFDC_107", "resource functions are not allowed in sfdc:Service", ERROR);
+
+    private final String code;
+    private final String message;
+    private final DiagnosticSeverity severity;
+
+    SfdcDiagnosticCodes(String code, String message, DiagnosticSeverity severity) {
+        this.code = code;
+        this.message = message;
+        this.severity = severity;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public DiagnosticSeverity getSeverity() {
+        return severity;
+    }
+}
---
+++ compiler-plugin/src/main/java/org/ballerinalang/sf/plugin/SfdcResourceValidator.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.ballerinalang.sf.plugin;
+
+import io.ballerina.compiler.syntax.tree.*;
+import io.ballerina.projects.plugins.SyntaxNodeAnalysisContext;
+import io.ballerina.tools.diagnostics.DiagnosticFactory;
+import io.ballerina.tools.diagnostics.DiagnosticInfo;
+
+import java.util.Optional;
+
+import static org.ballerinalang.sf.plugin.Constants.*;
+
+public class SfdcResourceValidator {
+    static void validateResource(SyntaxNodeAnalysisContext ctx, FunctionDefinitionNode member) {
+        extractRemoteMethodNameAndValidate(ctx, member);
+        extractResourceParametersAndValidate(ctx, member);
+        extractQualifierListAndValidate(ctx, member);
+    }
+
+    static void extractFunctionAnnotationAndValidate(SyntaxNodeAnalysisContext ctx, ServiceDeclarationNode member) {
+        Optional<MetadataNode> metadataNodeOptional = member.metadata();
+        if (metadataNodeOptional.isEmpty()) {
+            updateDiagnostic(ctx, member, "empty", SfdcDiagnosticCodes.SFDC_101);
+        } else {
+            MetadataNode metadataNode = metadataNodeOptional.orElseThrow();
+            NodeList<AnnotationNode> annotations = metadataNode.annotations();
+            for (AnnotationNode annotation : annotations) {
+                Node annotationReference = annotation.annotReference();
+                String annotationName = annotationReference.toString();
+                if (annotationReference.kind() == SyntaxKind.QUALIFIED_NAME_REFERENCE) {
+                    String[] strings = annotationName.split(":");
+                    if (SERVICE_CONFIG_ANNOTATION.equals(strings[strings.length - 1].trim())) {
+                        continue;
+                    }
+                }
+                updateDiagnostic(ctx, metadataNode, annotationName, SfdcDiagnosticCodes.SFDC_101);
+            }
+        }
+    }
+
+    private static void extractQualifierListAndValidate(SyntaxNodeAnalysisContext ctx, FunctionDefinitionNode member) {
+        NodeList<Token> tokens = member.qualifierList();
+        if (tokens.isEmpty()) {
+            updateDiagnostic(ctx, member, "remote", SfdcDiagnosticCodes.SFDC_106);
+        }
+    }
+
+    private static void extractRemoteMethodNameAndValidate(SyntaxNodeAnalysisContext ctx, FunctionDefinitionNode member) {
+        IdentifierToken functionNameToken = member.functionName();
+        String functionName = functionNameToken.toString().trim();
+        if (!(functionName.equals(ON_UPDATE) || functionName.equals(ON_CREATE) || functionName.equals(ON_DELETE) || functionName.equals(ON_RESTORE))) { // include other method names
+            updateDiagnostic(ctx, functionNameToken, functionName, SfdcDiagnosticCodes.SFDC_102);
+        }
+    }
+
+    private static void extractResourceParametersAndValidate(SyntaxNodeAnalysisContext ctx, FunctionDefinitionNode member) {
+        FunctionSignatureNode signatureNode = member.functionSignature();
+        SeparatedNodeList<ParameterNode> parameterList = signatureNode.parameters();
+        if (parameterList.isEmpty()) {
+            updateDiagnostic(ctx, signatureNode, "empty", SfdcDiagnosticCodes.SFDC_103);
+        } else if (parameterList.size() > 1) {
+            updateDiagnostic(ctx, signatureNode, "more than one", SfdcDiagnosticCodes.SFDC_104);
+        } else {
+            ParameterNode node = parameterList.get(0);
+            Node typeNameReference = ((RequiredParameterNode) node).typeName();
+            String typeName = typeNameReference.toString();
+            String[] strings = typeName.split(":");
+            if (!(EVENT_DATA_TYPE.equals(strings[strings.length - 1].trim()))) {
+                updateDiagnostic(ctx, signatureNode, typeName, SfdcDiagnosticCodes.SFDC_105);
+            }
+        }
+    }
+
+    private static void updateDiagnostic(SyntaxNodeAnalysisContext ctx, Node node, String argName,
+                                         SfdcDiagnosticCodes sfdcDiagnosticCodes) {
+        DiagnosticInfo diagnosticInfo = getDiagnosticInfo(sfdcDiagnosticCodes, argName);
+        ctx.reportDiagnostic(DiagnosticFactory.createDiagnostic(diagnosticInfo, node.location()));
+    }
+
+    private static DiagnosticInfo getDiagnosticInfo(SfdcDiagnosticCodes diagnostic, Object... args) {
+        return new DiagnosticInfo(diagnostic.getCode(), String.format(diagnostic.getMessage(), args),
+                diagnostic.getSeverity());
+    }
+}
